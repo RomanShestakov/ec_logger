343248634596f91273937692f640adb703550017
Roman Shestakov <romanshestakov@yahoo.co.uk>
2012-01-11 19:18:43 +0000

    allow multiple loggers

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $PWD/ebin -pa $PWD/deps/*/ebin -name ec_logger@127.0.0.1 \
+# create a random vm name so multiple loggers could be started
+exec erl -pa $PWD/ebin -pa $PWD/deps/*/ebin -name ec_logger$$"@127.0.0.1" \
     -setcookie rs -s ec_logger_app start -config ebin/ec_logger
