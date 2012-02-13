1c8824410647a742004548d24f2050ba87c80f73
Roman Shestakov <romanshestakov@yahoo.co.uk>
2012-02-13 19:36:48 +0000

    add hostname

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd `dirname $0`
 # create a random vm name so multiple loggers could be started
-exec erl -pa $PWD/ebin -pa $PWD/deps/*/ebin -name ec_logger$$"@127.0.0.1" \
+exec erl -pa $PWD/ebin -pa $PWD/deps/*/ebin -name ec_logger$$"@"$HOSTNAME \
     -setcookie rs -s ec_logger_app start -config ebin/ec_logger
